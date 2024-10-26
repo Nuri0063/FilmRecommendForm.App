1801e08f0e5bd33c119e84fe8761a75604a4014f
Nuri Işık <nurii6345@gmail.com>
2024-10-26 15:23:37 +0300

    #NuGet den EntityFrameworkCore.Jet eklentisini bütün katmanlar için indirdim

--- FilmRecommendForm.Data/obj/Debug/net8.0/FilmRecommendForm.Data.AssemblyInfo.cs
+++ FilmRecommendForm.Data/obj/Debug/net8.0/FilmRecommendForm.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmRecommendForm.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e594c054cb3693e504760b79fe60b53ed3103f10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0686b5877937bbddd9e18b35f6ff7ae985c0dbf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmRecommendForm.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmRecommendForm.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
